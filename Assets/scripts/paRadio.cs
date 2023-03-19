1407058d5ebeaf0b645dc49ff782a036a0852564
codec-xyz <dev@codec.wtf>
2023-03-18 23:49:40 -0400

    Fixes pa system

--- Assets/scripts/paRadio.cs
+++ Assets/scripts/paRadio.cs
@@ -9,13 +9,11 @@ public class paRadio : UdonSharpBehaviour
 	public Transform paRadioReset;
 
 	public void paStart() {
-		Networking.GetOwner(gameObject).SetVoiceVolumetricRadius(1000);
 		Networking.GetOwner(gameObject).SetVoiceDistanceNear(1000000);
 		Networking.GetOwner(gameObject).SetVoiceDistanceFar(1000000);
 	}
 
 	public void paEnd() {
-		Networking.GetOwner(gameObject).SetVoiceVolumetricRadius(0);
 		Networking.GetOwner(gameObject).SetVoiceDistanceNear(0);
 		Networking.GetOwner(gameObject).SetVoiceDistanceFar(25);
 		transform.position = paRadioReset.position;
